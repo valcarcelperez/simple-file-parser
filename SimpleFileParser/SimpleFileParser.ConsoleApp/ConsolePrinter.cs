241a23c50931a498166bd929465f4ee14f1019e6
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2020-09-01 20:21:13 -0400

    Completed Console app.

---
+++ SimpleFileParser/SimpleFileParser.ConsoleApp/ConsolePrinter.cs
@@ -0,0 +1,13 @@
+﻿using SimpleFileParser.Domain;
+using System;
+
+namespace SimpleFileParser.ConsoleApp
+{
+    public class ConsolePrinter : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
--- SimpleFileParser/SimpleFileParser.ConsoleApp/Program.cs
+++ SimpleFileParser/SimpleFileParser.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SimpleFileParser.Domain;
+using System;
+using System.IO;
 
 namespace SimpleFileParser.ConsoleApp
 {
@@ -6,7 +8,27 @@ namespace SimpleFileParser.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Path argument is missing. Please pass a path argument.");
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            var path = args[0];
+            if (!File.Exists(path))
+            {
+                // Note: FileParser is already validating the file but this is a better place.
+                Console.WriteLine($"File '{path}' not found");
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            var config = new FileParserConfig { Delimiter = ',', TargetField = 3, ExpectedFieldCount = 5 };
+            var printer = new ConsolePrinter();
+
+            var fileParser = new FileParser(printer, config);
+            fileParser.Parse(path);
         }
     }
 }
